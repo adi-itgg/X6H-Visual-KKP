30f43644a939613261cbf272eadc3fde7a6dc716
Putra <syahdilla.ps@gmail.com>
2025-03-09 21:22:11 +0700

    📝 Add: AppConfiguration props & getters
    - Adds a new private property `appName` to `AppConfiguration` class,
    - along with its getter method `appName()`.

--- src/main/java/io/aitech/pv/config/AppConfiguration.java
+++ src/main/java/io/aitech/pv/config/AppConfiguration.java
@@ -2,7 +2,13 @@ package io.aitech.pv.config;
 
 public class AppConfiguration {
 
+    private String appName;
     private PostgresConfiguration postgres;
+
+
+    public String appName() {
+        return appName;
+    }
 
     public PostgresConfiguration postgres() {
         return postgres;
